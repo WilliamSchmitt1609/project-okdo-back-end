a927f10723408775602b6301ab771785309a3e08
WilliamSchmitt1609 <williamschmitt16098877@gmail.com>
2022-02-03 16:34:50 +0100

    CRUD in progress

--- OKDO/src/Controller/Back/ProductController.php
+++ OKDO/src/Controller/Back/ProductController.php
@@ -3,8 +3,10 @@
 namespace App\Controller\Back;
 
 use DateTime;
+use App\Entity\Event;
 use App\Entity\Product;
 use App\Form\ProductType;
+use App\Repository\EventRepository;
 use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -30,9 +32,11 @@ class ProductController extends AbstractController
     /**
      * @Route("/new", name="back_product_new", methods={"GET", "POST"})
      */
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    public function new(Request $request, EntityManagerInterface $entityManager, EventRepository $eventRepository): Response
     {
         $product = new Product();
+        $event = $eventRepository->findBy(['product' => $product]);
+
         $form = $this->createForm(ProductType::class, $product);
         $form->handleRequest($request);
 
@@ -84,13 +88,30 @@ class ProductController extends AbstractController
     /**
      * @Route("/test/{id}", name="test", methods={"GET"})
      */
-
-    public function Test(ProductRepository $productRepository){
+   /*  public function Test(ProductRepository $product, Event $event){
     
-        $productList= $productRepository->findAllOrderedByTitleAscDql();
-        dd($productList);
+        $filteredProduct = $product->eventSearch($event);
 
+        dd($filteredProduct);
+
+    } */
+
+     /**
+     * @Route("/test1", name="test", methods={"GET"})
+     */
+    public function search(SerializerInterface $serializer, ProductRepository $ProductRepository, Request $request)
+    {
+        $profiles = $serializer->deserialize($request->getContent(), Profiles::class, 'json');
+        $products = $ProductRepository->findBy(['status' => 1], ['created_at' => 'desc'], 20);
+        
+
+        
+            $annonces = $ProductRepository->search(
+                $search->get('category')->getData()
+            );
+        
     }
+
 
     /**
      * @Route("/{id}", name="back_product_delete", methods={"POST"})
@@ -104,4 +125,6 @@ class ProductController extends AbstractController
 
         return $this->redirectToRoute('back_product_index', [], Response::HTTP_SEE_OTHER);
     }
+
+   
 }
